f6c67cd82374d55ce4d2b955ee5284eeb0c159db
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-20 20:51:50 +0100

    created files

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+ * cocktail_sort_list - sorts an array of integers.
+ * @list: doubly-linked list of integers to sort
+ */
+
+void cocktail_sort_list(listint_t **list) {
+
+}
