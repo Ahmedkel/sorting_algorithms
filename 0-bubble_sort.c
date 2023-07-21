993cd448cd8ec90435f97cd17629d97f916766d7
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-21 17:51:56 +0100

    edited

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,15 +1,33 @@
 #include "sort.h"
 
+#include <stdio.h>
+
 /**
- * swap - Swaps the values of two integers.
- * @a: Pointer to the first integer.
- * @b: Pointer to the second integer.
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
  */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
 void swap(int *a, int *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 /**
  * bubble_sort - sorts an array of integers.
@@ -19,15 +37,37 @@ void swap(int *a, int *b)
 
 void bubble_sort(int *array, size_t size)
 {
-	int i, j;
+    size_t i, j;
 
+	if(size < 2 || array == NULL)
+		return;
 	for (i = size - 1; i >= 1; i--)
 	{
 		for (j = 1; j <= i; j++)
 		{
 			if (array[j - 1] > array[j])
+			{
 				swap(&array[j - 1], &array[j]);
 				print_array(array, size);
+			}
 		}
 	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    bubble_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
 }
