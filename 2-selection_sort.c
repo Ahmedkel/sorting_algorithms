b1501c6768289e73883e0860857834663ee40aa0
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-07-22 19:22:44 +0100

    task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,11 +1,36 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * selection_sort - sorts an array of integers.
  * @array: array of integers to sort.
  * @size: size of the array.
  */
 
-void selection_sort(int *array, size_t size) {
+void selection_sort(int *array, size_t size)
+{
+	int i, j, min;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		if (array[j] < array[min])
+			min = j;
+		if (min != i)
+		swap(&array[i], &array[min]);
+		print_array(array, size);
+	}
 
 }
